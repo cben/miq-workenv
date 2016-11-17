a79050a4b43c737276a761f2248ec1fd1c2f60e0
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-11-17 11:07:11 +0200

    Some files I use above multiple ManageIQ worktrees.

---
+++ Gemfile.dev.rb
@@ -0,0 +1,10 @@
+gem "pry"
+gem "pry-doc"
+gem "pry-rails"
+gem "pry-remote"
+gem "pry-byebug"
+gem "byebug"
+gem "rb-readline"  # for irb
+#gem "rubocop"
+gem "haml-lint"
+gem "launchy"  # TODO: https://stackoverflow.com/questions/35383550/how-to-automatically-launch-browser-when-rails-server-starts
