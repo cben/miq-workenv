eb563c4bcdffdb609f0eebb8dda79d373efa7f80
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-12-25 14:43:00 +0200

    Depend on manageiq-ui-classic, with relative path
    Could be simplified if my fix to manageiq-ui-classic/Gemfile gets merged.

--- Gemfile.dev.rb
+++ Gemfile.dev.rb
@@ -1,3 +1,9 @@
+# https://github.com/ManageIQ/guides/blob/master/developer_setup/classic_ui_split.md
+# This file gets evaluated both directly from manageiq directory, and from manageiq-ui-classic directory
+# via manageiq-ui-classic/spec symlink, which may cause funny errors.
+gem "manageiq-ui-classic", :path => File.realpath("../manageiq-ui-classic", __dir__)
+#gem "manageiq-ui-classic", :path => "../manageiq-ui-classic"
+
 gem "pry"
 gem "pry-doc"
 gem "pry-rails"
@@ -6,5 +12,6 @@ gem "pry-byebug"
 gem "byebug"
 gem "rb-readline"  # for irb
 #gem "rubocop"
+gem "rubocop-git"
 gem "haml-lint"
 gem "launchy"  # TODO: https://stackoverflow.com/questions/35383550/how-to-automatically-launch-browser-when-rails-server-starts
